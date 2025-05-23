cdb40bb5dd7a6f308a5cff9e747dfc8761d82449
Vadim Gvozdev <vad.gvozdew@yandex.ru>
2025-05-24 02:38:00 +0300

    fix combine.tcl

--- building-sin+dna/combine.tcl
+++ building-sin+dna/combine.tcl
@@ -1,7 +1,7 @@
 package require psfgen
 resetpsf
-readpsf ../building-dna/polys_autopsf.psf
-coordpdb ../building-dna/polys_autopsf.pdb
+readpsf ../building-dna/dsdna.psf
+coordpdb ../building-dna/dsdna.pdb
 readpsf ../building-sin/sin_pore_charges.psf
 coordpdb ../building-sin/sin_pore_charges.pdb
 writepsf sin+dna.psf
